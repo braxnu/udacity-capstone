86ccf1d656c983ca8357f8d007661758552f648b
braxnu <braxnu@gmail.com>
2022-11-07 15:09:54 +0000

    working node app

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  // parserOptions: {
+  //   tsconfigRootDir: __dirname,
+  //   project: ['./tsconfig.json'],
+  // },
+  plugins: ['@typescript-eslint'],
+  root: true,
+}
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import fastify from 'fastify'
+
+const app = fastify()
+const port = Number(process.env.PORT) || 3000
+
+app.get('/', async (req, reply) => {
+  reply.send('ok')
+})
+
+;(async () => {
+  await app.listen({port})
+  console.log(`Server started on http://127.0.0.1:${port}`)
+})()
